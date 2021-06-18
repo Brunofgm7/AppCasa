880774b5cd41efd47997028a8ab566b38309ac3f
Bruno Moreira <bruno7moreira@gmail.com>
2021-06-18 12:30:25 +0100

    adição do icon da app e outros ajustes

--- app/src/main/java/com/example/appcasa/ServiceBuilder.kt
+++ app/src/main/java/com/example/appcasa/ServiceBuilder.kt
@@ -6,7 +6,7 @@ import retrofit2.converter.gson.GsonConverterFactory
 
 object ServiceBuilder {
 
-    private const val URL = "http://192.168.1.76:3000/"
+    private const val URL = "http://10.0.2.2:3000/"
 
     private val okHttp = OkHttpClient.Builder()
 
